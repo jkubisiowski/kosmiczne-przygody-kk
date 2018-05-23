e669ef77a41168eab54af8e434ea64086311d463
Jakub Kubisiowski <jimjam.sg@gmail.com>
2018-05-23 12:44:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //var target_date = new Date().getTime() + (1000*3600*48); // set the countdown date
-var target_date = new Date("2018/05/25 18:00:00");
+var target_date = new Date("2018/05/25 16:00:00");
 var days, hours, minutes, seconds; // variables for time units
 
 var countdown = document.getElementById("tiles"); // get tag element
